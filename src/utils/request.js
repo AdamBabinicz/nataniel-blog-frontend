e15a02a7c7230ee482c5f76a945a6486fee4950e
babinicz <babinicz@onet.pl>
2023-04-08 20:33:39 +0200

    zm

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const request = axios.create({
+//   baseURL: "http://localhost:8000",
+// });
+
 const request = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://nat-xbs5.onrender.com",
 });
 
 export default request;
