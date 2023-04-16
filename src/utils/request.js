211a351ec14b3011cc01cd4719f83ae76124bec6
babinicz <babinicz@onet.pl>
2023-04-16 15:02:04 +0200

    zm

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://blog-nataniel.netlify.app",
+  baseURL: "http://localhost:8000",
 });
 
 export default request;
