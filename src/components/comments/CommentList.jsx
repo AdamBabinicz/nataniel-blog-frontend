849fe4010fc57d339249fd99d3e343660689f999
babinicz <babinicz@onet.pl>
2023-04-30 16:58:47 +0200

    brak moment

--- src/components/comments/CommentList.jsx
+++ src/components/comments/CommentList.jsx
@@ -6,7 +6,7 @@ import Moment from "react-moment";
 // import "moment/locale/pl";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteComment } from "../../redux/apiCalls/commentApiCall";
-import(`moment/locale/${navigator.language.toLocaleLowerCase()}`).then();
+// import(`moment/locale/${navigator.language.toLocaleLowerCase()}`).then();
 
 const CommentList = ({ comments }) => {
   const dispatch = useDispatch();
