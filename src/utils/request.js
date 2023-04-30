140cadc786eea174c187e9e8741f822e282adc8a
babinicz <babinicz@onet.pl>
2023-04-30 18:21:24 +0200

    komentarz

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const request = axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: "https://natanek.onrender.com",
+  baseURL: "https://natanek.onrender.com", // backend
 });
 
 export default request;
